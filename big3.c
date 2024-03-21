085a14f8042487ee10d67cc80fd0b1a850ccc502
Apekshshn <127499924+Apekshshn@users.noreply.github.com>
2024-03-21 19:22:52 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -4,20 +4,20 @@
 
 int biggest3()
 {
-	int A, B, c;
+	int A, B, C;
 
-	printf("Enter the numbers A, B and c: ");
-	scanf("%d %d %d", &A, &B, &c);
+	printf("Enter the numbers A, B and C: ");
+	scanf("%d %d %d", &A, &B, &C);
 
 	// finding max using 
-	if (A >= B && A >= c)
+	if (A >= B && A >= C)
 		printf("%d is the largest number.", A);
 
-	else if (B >= A && B >= c)
+	else if (B >= A && B >= C)
 		printf("%d is the largest number.", B);
 
 	else
-		printf("%d is the largest number.", c);
+		printf("%d is the largest number.", C);
 
 	//return 0;
 }
